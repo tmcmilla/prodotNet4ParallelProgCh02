4d107b6cd356b90927f8e2bab9804d5433e497d8
tmcmilla <thomasc.mcmillan@gmail.com>
2017-01-13 09:35:20 -0800

    Add project files.

---
+++ Listing_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Task.Factory.StartNew(() =>
+     {
+       Console.WriteLine("Hello World");
+     });
+      // wait for input before exiting
+      Console.WriteLine("'Main method complete. Press enter to Finish");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Listing_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // use an aAction delegate and a named method
+      Task task1 = new Task(new Action(printMessage));
+    }
+    static void printMessage()
+    {
+
+    }
+  }
+}
